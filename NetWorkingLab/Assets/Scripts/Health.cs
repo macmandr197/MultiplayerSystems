ef610c3940918bafa94ad094d9973e0683d4b8bd
Andrew <andrewmacmillan1@hotmail.com>
2018-03-15 18:54:46 -0400

    synced up the healthbar UI with clients and server

--- NetWorkingLab/Assets/Scripts/Health.cs
+++ NetWorkingLab/Assets/Scripts/Health.cs
@@ -1,24 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 using UnityEngine;
 
-public class Health : MonoBehaviour
+public class Health : NetworkBehaviour
 {
     public RectTransform healthBar;
 
 
     public const int MaxHealth = 100;
+    [SyncVar(hook = "OnChangedHealth")]
     public int currentHealth = MaxHealth;
 
     public void TakeDamage(int amount)
     {
+        if (!isServer)
+        {
+            return;
+        }
         currentHealth -= amount;
         if (currentHealth <= 0)
         {
             currentHealth = 0;
             Debug.Log("Dead!");
         }
-        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    }
+
+    void OnChangedHealth(int health)
+    {
+        healthBar.sizeDelta = new Vector2(health, healthBar.sizeDelta.y);
     }
 }
