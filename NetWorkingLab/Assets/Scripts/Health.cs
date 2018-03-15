8d0ff82c848cdd24c10b21e918aa4854be917380
Andrew <andrewmacmillan1@hotmail.com>
2018-03-15 18:47:56 -0400

    added healthbar to player in order to visualize health decrementation

---
+++ NetWorkingLab/Assets/Scripts/Health.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public RectTransform healthBar;
+
+
+    public const int MaxHealth = 100;
+    public int currentHealth = MaxHealth;
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Debug.Log("Dead!");
+        }
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    }
+}
