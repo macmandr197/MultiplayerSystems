d807fd530621078020dcaf12d0fde44a67c12d7d
Andrew <andrewmacmillan1@hotmail.com>
2018-04-09 20:14:56 -0400

    Added beginnings of custom network manager. Added starter player prefab, and flagpole prefab. Added a few materials

---
+++ FinalProject/Assets/Scripts/NetworkManager_Custom.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class NetworkManager_Custom : NetworkManager
+{
+    public void StartupJost()
+    {
+        SetPort();
+        singleton.StartHost();
+    }
+
+    public void JoinGame()
+    {
+        SetIPAddress();
+        SetPort();
+
+        NetworkManager.singleton.StartClient();
+    }
+
+    void SetIPAddress()
+    {
+        string ipAddress = GameObject.Find("InputFieldIPAddress").transform.Find("Text").GetComponent<Text>().text;
+        NetworkManager.singleton.networkAddress = ipAddress;
+    }
+
+    void SetPort()
+    {
+        NetworkManager.singleton.networkPort = 7777;
+    }
+}
