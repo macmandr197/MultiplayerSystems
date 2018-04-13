2712500d74c888612bfac9e2025b20f0bb798d6e
Andrew <andrewmacmillan1@hotmail.com>
2018-04-12 21:43:15 -0400

    Imported standard assets, created player movement controller, rotation and positional syncing with server, Id association scripts

---
+++ FinalProject/Assets/Scripts/Player_NetworkSetup.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player_NetworkSetup : NetworkBehaviour
+{
+
+    [SerializeField] Camera FPSCharacterCam;
+    [SerializeField] AudioListener audioListener;
+
+    // Use this for initialization
+    public override void OnStartLocalPlayer()
+    {
+        GameObject.Find("Scene Camera").SetActive(false);
+        //GetComponent<CharacterController>().enabled = true;
+        GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = true;
+        FPSCharacterCam.enabled = true;
+        //audioListener.enabled = true;
+
+        Renderer[] rens = GetComponentsInChildren<Renderer>();
+        foreach (Renderer ren in rens)
+        {
+            ren.enabled = false;
+        }
+
+        //GetComponent<NetworkAnimator>().SetParameterAutoSend(0, true);
+    }
+
+    public override void PreStartClient()
+    {
+        //GetComponent<NetworkAnimator>().SetParameterAutoSend(0, true);
+    }
+
+}
